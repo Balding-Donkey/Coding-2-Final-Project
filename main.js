ac2a1fa0215aa51bbed2409c97cedb307fe74742
Balding Donkey <s1053436@student.knoxschools.org>
2026-04-08 17:11:40 +0000

    Set up basic page with a canvas

--- main.js
+++ main.js
@@ -1,1 +1,6 @@
-print("Hello World!")
+let text = document.getElementById("text");
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+
+ctx.font = "30px Courier New";
+ctx.fillRect(0, 0, canvas.width, canvas.height);
