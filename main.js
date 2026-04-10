c74ec8d53857d95c03fa4776e83cf6f063da6f62
Balding Donkey <s1053436@student.knoxschools.org>
2026-04-10 20:05:42 +0000

    Debug console added, tile rendering partially implemented.

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
-let text = document.getElementById("text");
-let canvas = document.getElementById("canvas");
-let ctx = canvas.getContext("2d");
-
-let t = 0;
+// Error logging and output to get around the fact that I can't use the developer console
+let textLog = document.getElementById("textLog");
+function output(string) {
+    textLog.innerHTML = textLog.innerHTML + string + "<br>";
+}
+window.addEventListener("error", (event) => {
+    output("Error at " + event.filename + ":" + event.lineno + " - " + event.message);
+});
 
 
 // Keyboard input
@@ -29,6 +32,9 @@ function resetHitKeys() {
     }
 }
 
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+let t = 0;
 
 // // Object classes, will set up later
 // class dynamicObject {
@@ -65,11 +71,52 @@ function resetHitKeys() {
 //     }
 // }
 
+let levelWidth = 20;
+let levelHeight = 20;
+const DEFAULT_TILE = "grass";
+let levelTiles = new Array(levelWidth).fill(null).map(() => new Array(levelHeight).fill(DEFAULT_TILE));
+levelTiles[0][5] = "dirt";
+output(levelTiles);
+
+let cameraX = 0;
+let cameraY = 0;
+let cameraZoom = 50;
+
 
 
 function drawRectangle(x, y, width, height) {
     ctx.fillRect(x - width / 2, y - height / 2, width, height);
 }
+
+let tileCtx = {
+    x: 0,
+    y: 0,
+    width: 50,
+    height: 50
+}
+function drawRectangleInTileContext(x, y, width, height) {
+    drawRectangle(tileCtx.x + x * tileCtx.width, tileCtx.y + y * tileCtx.height, width * tileCtx.width, height * tileCtx.height);
+}
+
+class tile {
+    constructor() {
+        this.collidable = false;
+        this.layer = 0;
+    }
+
+    draw(x, y) {
+        return
+    }
+}
+
+
+// function drawTile(x, y, tileType) {
+//     tileCtx.x = cameraX;
+//     tileCtx.y = cameraY;
+//     tileCtx.width = cameraZoom;
+//     tileCtx.height = cameraZoom;
+//     tileTypes[tileType].draw(x, y);
+// }
 
 function renderFrame() {
     // Clear the canvas for the new frame
@@ -77,16 +124,25 @@ function renderFrame() {
 
     let x = 0;
     ctx.font = "20px Arial";
-    // Render all pressed keys as a test
+    // Render all pressed keys as text
     for (let key in keyDown) {
         if (keyDown[key]) {
             ctx.fillText(key, x, canvas.height / 2);
             x += 30;
         }
     }
+
+    // Draw the tiles
+    // for (let x = 0; x < levelWidth; x++) {
+    //     for (let y = 0; y < levelHeight; y++) {
+    //         let tileType = levelTiles[x][y];
+    //         tileTypes[tileType].draw(x, y);
+    //     }
+    // }
 }
 
 function updateGame() {
+    // output(t);
 }
 
 function gameTick() {
