9a70d297ed964e660a9fc40ad4f367d730371c51
Balding Donkey <s1053436@student.knoxschools.org>
2026-04-17 20:27:03 +0000

    Improved input system in preparation for custom keybinds, added object base classes and player class, set up collision detection, but it does not push objects out. There are also more comments and various other changes.

--- main.js
+++ main.js
@@ -3,9 +3,18 @@ let textLog = document.getElementById("textLog");
 function output(string) {
     textLog.innerHTML = textLog.innerHTML + string + "<br>";
 }
+output("Script loaded successfully"); // If this does not appear then if means the script didn't run
 window.addEventListener("error", (event) => {
     output("Error at " + event.filename + ":" + event.lineno + " - " + event.message);
 });
+
+
+// Math
+function logBase(base, x) {
+    // Finish later
+    return
+}
+
 
 
 // Import assets
@@ -15,6 +24,7 @@ function loadAsset(name, path) {
     image.src = "assets/" + path;
     assets[name] = image;
 }
+// Placeholder assets
 loadAsset("tiller", "partick tiler.png");
 loadAsset("amongus", "sussy.png");
 loadAsset("carrots", "Carrots.png");
@@ -24,6 +34,7 @@ loadAsset("flag", "Flag_R.png");
 
 let log = document.getElementById("log");
 let logButton = document.getElementById("loggleToggle");
+// Allows you to hide and show the log
 logButton.addEventListener("click", () => {
     if (log.style.display === "none") {
         log.style.display = "block";
@@ -32,11 +43,20 @@ logButton.addEventListener("click", () => {
     }
 });
 
+// Canvas setup
+let canvas = document.getElementById("canvas");
+let ctx = canvas.getContext("2d");
+ctx.imageSmoothingEnabled = false; // Allows for pixelated graphics to be scaled properly
+let t = 0; // The amount of frames that have run so far
+let canvasWidth = canvas.width; // Should be the actual width of the element, but can be changed to "look outside" the canvas
+let canvasHeight = canvas.height; // Same as above
+ctx.translate((canvas.width - canvasWidth) / 2, (canvas.height - canvasHeight) / 2); // Only has an effect if the canvasWidth or canvasHeight variables differ from the actual size
+
 
 // Keyboard input
-let keyDown = {};
-let keyPressed = {};
-let keyHit = {};
+let keyDown = {}; // A map of keys that are currently held down
+let keyPressed = {}; // A map of keys which have been pressed this frame, behaves like typing, so holding one will cause it to fire repeatedly
+let keyHit = {}; // A map of which keys have been hit this frame (true for one frame)
 
 window.addEventListener("keydown", (event) => {
     if (!keyDown[event.key]) {
@@ -49,7 +69,41 @@ window.addEventListener("keyup", (event) => {
     keyDown[event.key] = false;
 });
 
-function resetHitKeys() {
+// Mouse input
+let mouseWorldPos = {x: 0, y: 0}; // The position of the mouse, relative to ingame coordinates
+let mouseGridPos = {x: 0, y: 0}; // The position of the mouse rounded to the grid
+let mouseX = 0; // The screenspace X position of the mouse
+let mouseY = 0; // The screenspace Y position of the mouse
+
+canvas.addEventListener("mousemove", (event) => {
+    let rect = canvas.getBoundingClientRect();
+    mouseX = event.clientX - rect.left - canvasWidth / 2;
+    mouseY = event.clientY - rect.top - canvasHeight / 2;
+    // mouseWorldPos = transformToWorldSpace(mouseX, mouseY); // Do this in the game loop instead so that it happens when the camera is moved too
+});
+
+let mouseDown = false; // Whether the user is currently holding the primary mouse button
+let mouseClicked = false; // Whether the user clicked the primary mouse button this frame (true for one frame)
+// Uses canvas so that offscreen clicks won't be registered.
+canvas.addEventListener("mousedown", (event) => {
+    if (event.button !== 0) {
+        return;
+    }
+    mouseDown = true;
+    mouseClicked = true;
+});
+/* Uses document instead of canvas so the click won't stay registered if the user moses the mouse off the canvas.
+This still allows the user to keep the mouse held by switching tabs, which is an intentional choice. */
+document.addEventListener("mouseup", (event) => {
+    if (event.button !== 0) {
+        return;
+    }
+    mouseDown = false;
+});
+
+// Reset keys hit and mouse clicked each frame
+function resetInput() {
+    mouseClicked = false;
     for (let key in keyPressed) {
         keyPressed[key] = false;
     }
@@ -58,56 +112,30 @@ function resetHitKeys() {
     }
 }
 
+let userInput = {
+    "moveUp": false,
+    "moveDown": false,
+    "moveLeft": false,
+    "moveRight": false,
+    "jump": false,
+    "zoomIn": false,
+    "zoomOut": false,
+};
+function updateBindableInputs() {
+    // Handles inputs with multiple or changeable binds
+    // Binds being set by variables will be added later
+    userInput["moveUp"] = ((keyDown["w"] ?? false) || (keyDown["ArrowUp"] ?? false));
+    userInput["moveDown"] = ((keyDown["s"] ?? false) || (keyDown["ArrowDown"] ?? false));
+    userInput["moveLeft"] = ((keyDown["a"] ?? false) || (keyDown["ArrowLeft"] ?? false));
+    userInput["moveRight"] = ((keyDown["d"] ?? false) || (keyDown["ArrowRight"] ?? false));
+    userInput["jump"] = ((keyHit[" "] ?? false) || (keyHit["z"] ?? false));
+    userInput["zoomIn"] = (keyDown["e"] ?? false);
+    userInput["zoomOut"] = (keyDown["q"] ?? false);
+}
 
-let canvas = document.getElementById("canvas");
-let mouseWorldPos = {x: 0, y: 0};
-let mouseX = 0;
-let mouseY = 0;
-canvas.addEventListener("mousemove", (event) => {
-    let rect = canvas.getBoundingClientRect();
-    mouseX = event.clientX - rect.left - canvas.width / 2;
-    mouseY = event.clientY - rect.top - canvas.height / 2;
-    // mouseWorldPos = transformToWorldSpace(mouseX, mouseY); // Do this the game loop instead so that it happens when the camera is moved too
-});
 
-let ctx = canvas.getContext("2d");
-ctx.imageSmoothingEnabled = false;
-let t = 0;
 
-// // Object classes, will set up later
-// class dynamicObject {
-//     constructor() {
-//         this.x = 0;
-//         this.y = 0;
-//     }
-
-//     update() {
-//         return
-//     }
-// }
-
-// class physicalObject extends dynamicObject {
-//     constructor() {
-//         super();
-//         this.vx = 0;
-//         this.vy = 0;
-//     }
-
-//     update() {
-//         this.x += this.vx;
-//         this.y += this.vy;
-//     }
-// }
-
-// class player extends physicalObject {
-//     constructor() {
-//         super();
-//     }
-
-//     update() {
-//         return
-//     }
-// }
+// Tiles
 
 class tile {
     constructor(sprite = null, collidable = false) {
@@ -123,9 +151,7 @@ let tileTypes = {
     "flower": new tile(assets["carrots"], false),
 }
 
-const DEFAULT_TILE = {
-    type: "grass"
-};
+const DEFAULT_TILE = {type: "air"};
 
 let levelWidth = 100;
 let levelHeight = 100;
@@ -136,24 +162,132 @@ for (let x = 0; x < levelWidth; x++) {
         if (Math.random() < 0.1) {
             levelTiles[0][x][y] = {type: "dirt"};
         }
-        if (Math.random() < 0.01) {
+        if (Math.random() < 0.02) {
             levelTiles[0][x][y] = {type: "flower"};
         }
     }
 }
 
 
+
+// Objects
+
+class dynamicObject {
+    // Base class for objects that can move, does not include physics
+    constructor() {
+        this.x = 0;
+        this.y = 0;
+        this.width = 1;
+        this.height = 1;
+}
+
+    update() {
+        return
+    }
+
+    render() {
+        return
+    }
+}
+
+function isSolidAt(z, x, y) {
+    if (x < 0 || x >= levelWidth || y < 0 || y > levelHeight) {
+        return false // In the case of being out of bounds
+    }
+    return tileTypes[levelTiles[z][Math.round(x)][Math.round(y)].type].collidable
+}
+
+function collisionGrid(z, x, y, width, height) {
+    const xLow = Math.round(x - width / 2);
+    const xHigh = Math.round(x + width / 2);
+    const yLow = Math.round(y - height / 2);
+    const yHigh = Math.round(y + height / 2);
+    let results = []
+    for (let x = xLow; x <= xHigh; x++) {
+        for (let y = yLow; y <= yHigh; y++) {
+            results.push(isSolidAt(0, x, y))
+        }
+    }
+    return results
+}
+
+class physicalObject extends dynamicObject {
+    // Base class for objects that collide with walls and experience gravity
+    constructor() {
+        super();
+        this.vx = 0;
+        this.vy = 0;
+        this.gravityY = 1;
+        this.friction = 0.7;
+    }
+
+    update() {
+        super.update();
+        if (Math.abs(this.vx) < 0.000001) {
+            this.vx = 0;
+        } else {
+            this.vx *= this.friction;
+        }
+        if (Math.abs(this.vy) < 0.000001) {
+            this.vy = 0;
+        } else {
+            this.vy *= this.friction
+        }
+        this.x += this.vx;
+        this.y += this.vy;
+        let collisionResults = collisionGrid(0, this.x, this.y, this.width, this.height);
+        if (collisionResults.includes(true)) {
+            this.vx = 0;
+            this.vy = 0;
+        }
+    }
+
+    render() {
+        return
+    }
+}
+
+class playerObject extends physicalObject {
+    // Player object controlled by the user
+    constructor() {
+        super();
+        this.walkSpeed = 0.1; // The maximum speed the player will move at
+        this.acceleration = (this.walkSpeed * (1 - this.friction) / this.friction); // This formula calculates the acceleration needed to reach the max speed with the specified friction
+        this.width = 1.5;
+        this.height = 1.5;
+    }
+
+    update() {
+        this.vx += this.acceleration * (userInput["moveRight"] - userInput["moveLeft"]);
+        this.vy += this.acceleration * (userInput["moveDown"] - userInput["moveUp"]);
+        super.update();
+    }
+
+    render() {
+        drawInWorldSpace(this.x, this.y, this.width, this.height, assets["tile"]);
+    }
+}
+
+
+// Required objects
+let player = new playerObject();
+let levelObjects = [player];
+
+
+// Camera variables
 let cameraX = levelWidth / 2;
 let cameraY = levelHeight / 2;
-let cameraZoom = 50;
+let cameraZoom = 70;
 
+
+// Rendering
 
 function drawCenteredRectangle(x, y, width, height) {
     ctx.fillRect(x - width / 2, y - height / 2, width, height);
 }
 
 function drawCenteredImage(image, x, y, width, height) {
-    ctx.drawImage(image, x - width / 2 + canvas.width / 2, y - height / 2 + canvas.height / 2, width, height);
+    ctx.drawImage(image, x - width / 2 + canvasWidth / 2, y - height / 2 + canvasHeight / 2, width, height);
 }
 
 function transformToWorldSpace(x, y) {
@@ -180,7 +314,7 @@ function drawInWorldSpace(x, y, width, height, image=null) {
     y = transformed.y;
     width = transformed.width;
     height = transformed.height;
-    if (x + width / 2 < -canvas.width / 2 || x - width / 2 > canvas.width / 2 || y + height / 2 < -canvas.height / 2 || y - height / 2 > canvas.height / 2) {
+    if (x + width / 2 < -canvasWidth / 2 || x - width / 2 > canvasWidth / 2 || y + height / 2 < -canvasHeight / 2 || y - height / 2 > canvasHeight / 2) {
         return;
     }
 
@@ -195,53 +329,86 @@ function drawInWorldSpace(x, y, width, height, image=null) {
 
 function renderFrame() {
     // Clear the canvas for the new frame
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-    // Render all pressed keys as text
-    // let x = 0;
-    // ctx.font = "20px Arial";
-    // for (let key in keyDown) {
-    //     if (keyDown[key]) {
-    //         ctx.fillText(key, x, canvas.height / 2);
-    //         x += 30;
-    //     }
-    // }
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
 
     // Draw the tiles
     for (let x = 0; x < levelWidth; x++) {
         for (let y = 0; y < levelHeight; y++) {
             let tileType = levelTiles[0][x][y].type;
-            drawInWorldSpace(x, y, 1, 1, tileTypes[tileType].sprite);
+            let tileSprite = tileTypes[tileType].sprite;
+            if (tileSprite === null) {
+                continue
+            }
+            drawInWorldSpace(x, y, 1, 1, tileSprite);
         }
     }
-    screenPos = transformFromWorldSpace(mouseWorldPos.x, mouseWorldPos.y);
-    // output("Screen position: " + screenPos.x + ", " + screenPos.y);
-    drawCenteredImage(assets["tiller"], screenPos.x, screenPos.y, cameraZoom, cameraZoom);
+
+    for (let i = 0; i < levelObjects.length; i++) {
+        levelObjects[i].render();
+    }
+
+    // // Render all pressed keys as text
+    // let x = 0;
+    // ctx.font = "20px Arial";
+    // for (let key in keyDown) {
+    //     if (keyDown[key]) {
+    //         ctx.fillText(key, x, canvasHeight / 2);
+    //         x += 30;
+    //     }
+    // }
 }
+
+
+
+// Basic game logic
 
 let panSpeed = 0.1;
 let zoomSpeed = 0.02;
+let freeCam = false;
+
 function updateGame() {
-    panSpeed = 5 / cameraZoom;
-    cameraY += panSpeed * ((keyDown["s"] ?? false) - (keyDown["w"] ?? false));
-    cameraX += panSpeed * ((keyDown["d"] ?? false) - (keyDown["a"] ?? false));
-    if (keyDown["e"]) {
-        cameraZoom *= 1 + zoomSpeed;
+    for (let i = 0; i < levelObjects.length; i++) {
+        levelObjects[i].update();
     }
-    if (keyDown["q"]) {
-        cameraZoom /= 1 + zoomSpeed;
+
+    // Free camera movement
+    if (freeCam) {
+        panSpeed = 5 / cameraZoom;
+        cameraY += panSpeed * (userInput["moveDown"] - userInput["moveUp"]);
+        cameraX += panSpeed * (userInput["moveRight"] - userInput["moveLeft"]);
+        if (userInput["zoomIn"]) {
+            cameraZoom *= 1 + zoomSpeed;
+        }
+        if (userInput["zoomOut"]) {
+            cameraZoom /= 1 + zoomSpeed;
+        }
+    } else {
+        cameraX = player.x;
+        cameraY = player.y;
     }
+
     mouseWorldPos = transformToWorldSpace(mouseX, mouseY);
+    mouseGridPos.x = Math.round(mouseWorldPos.x);
+    mouseGridPos.y = Math.round(mouseWorldPos.y);
+    if (mouseDown && mouseGridPos.x >= 0 && mouseGridPos.x < levelWidth && mouseGridPos.y >= 0 && mouseGridPos.y < levelHeight) {
+        levelTiles[0][mouseGridPos.x][mouseGridPos.y] = {type: "flower"};
+    }
 }
 
+
+// Game loop
 function gameTick() {
+    // Here we increment the frame counter, update user input, update the game state, render the game, reset the user input, and request the next frame.
     t += 1;
+    updateBindableInputs();
     updateGame();
     renderFrame();
-    resetHitKeys();
+    resetInput();
     requestAnimationFrame(gameTick);
 }
 
+
+// Wait until the assets are loaded before starting the game loop
 window.addEventListener("load", () => {
     requestAnimationFrame(gameTick);
 });
